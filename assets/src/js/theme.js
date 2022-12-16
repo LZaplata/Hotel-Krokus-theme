14a03a8d88cc78e99ece83843245d9ca2932bdf0
Lukáš Záplata <lukas.zaplata@gmail.com>
2022-12-16 13:17:24 +0100

    theme initialization

--- assets/src/js/theme.js
+++ assets/src/js/theme.js
@@ -1,6 +1,7 @@
 import lightGallery from "lightgallery";
 import lgThumbnail from "lightgallery/plugins/thumbnail"
 import lgZoom from "lightgallery/plugins/zoom"
+import flatpickr from "flatpickr";
 
 window.lightGallery = lightGallery;
 window.lgThumbnail = lgThumbnail;
@@ -12,6 +13,34 @@ lightGallery(document.getElementById("gallery"), {
     selector: ".swiper-image",
     plugins: [lgZoom, lgThumbnail],
     speed: 500,
+    galleryId: 1,
+});
+
+lightGallery(document.getElementById("gallery"), {
+    selector: ".gallery-image",
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+    galleryId: 1,
+});
+
+lightGallery(document.getElementById("menu"), {
+    selector: ".menu-image",
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+    galleryId: 2,
+});
+
+lightGallery(document.getElementById("rooms"), {
+    selector: ".swiper-image",
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+});
+
+var roomGallerySwiper = new Swiper(".room-gallery-swiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
 
 var gallerySwiper = new Swiper(".gallery-swiper", {
@@ -38,6 +67,37 @@ var gallerySwiper = new Swiper(".gallery-swiper", {
             slidesOffsetAfter: $(".mi-logo").offset().left - 12,
         },
     },
+});
+
+var today = new Date();
+var tomorrow = new Date();
+
+tomorrow.setDate(today.getDate() + 1);
+
+flatpickr(".datepicker", {
+    dateFormat: "j. n. Y",
+    defaultDate: "today",
+});
+
+flatpickr(".datepicker_to", {
+    dateFormat: "j. n. Y",
+    defaultDate: tomorrow,
+});
+
+flatpickr(".timepicker_from", {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true,
+    defaultDate: today.getHours() + ":" + today.getMinutes(),
+});
+
+flatpickr(".timepicker_to", {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true,
+    defaultDate: (today.getHours() + 1) + ":" + today.getMinutes(),
 });
 
 $(document).ready(function () {
